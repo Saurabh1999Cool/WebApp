f78c3255c52826f66ba1dc04b1959d553129a45c
Saurabh1999Cool <saurabh.gangajaliya@groupbayport.com>
2024-06-01 14:32:24 +0530

    script.js

--- js/script.js
+++ js/script.js
@@ -1,24 +1,35 @@
 document.getElementById('uploadForm').addEventListener('submit', function(event) {
     event.preventDefault();
     
-    var formData = new FormData(this);
+    var formData = new FormData();
     var fileInput = document.querySelector('input[type="file"]');
     formData.append('file', fileInput.files[0]);
-    
+
     fetch('https://saurabh5699.pythonanywhere.com/upload', {
         method: 'POST',
         body: formData
     })
-    .then(response => response.json())
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok ' + response.statusText);
+        }
+        return response.json();
+    })
     .then(data => {
+        console.log('Received data:', data);  // Log the received data
+
+        if (!data.shift_schedule || !Array.isArray(data.shift_schedule)) {
+            throw new Error('Expected an array under key "shift_schedule" but got: ' + JSON.stringify(data));
+        }
+
         // Display JSON data as a table
         var tableHtml = '<table>';
         tableHtml += '<thead><tr>';
-        for (var key in data[0]) {
+        for (var key in data.shift_schedule[0]) {
             tableHtml += '<th>' + key + '</th>';
         }
         tableHtml += '</tr></thead><tbody>';
-        data.forEach(function(row) {
+        data.shift_schedule.forEach(function(row) {
             tableHtml += '<tr>';
             for (var key in row) {
                 tableHtml += '<td>' + row[key] + '</td>';
@@ -28,5 +39,8 @@ document.getElementById('uploadForm').addEventListener('submit', function(event)
         tableHtml += '</tbody></table>';
         document.getElementById('tableContainer').innerHTML = tableHtml;
     })
-    .catch(error => console.error('Error:', error));
+    .catch(error => {
+        console.error('Error:', error);
+        alert('There was an error with the fetch operation: ' + error.message);
+    });
 });
